276037e3dccf5c1dd967d16c24996996b9963f6d
zxx <xin.zhang@robotrak.cn>
2023-05-22 16:04:22 +0800

    fix: bug

--- code/src/index.ts
+++ code/src/index.ts
@@ -31,12 +31,12 @@ app.listen(9000);
 app.get('/data', async (req, res) => {
   try {
     // @ts-ignore
-    log(req.query?.username || '');
-    log(req.query?.password || '');
-    let data = await getData();
+    const username = (req.query?.username || '') as string;
+    const password = (req.query?.password || '') as string;
+    let data = await getData(username, password);
     if (data === void 0) {
       log('获取不到匹配数据，强制更新token再获取一次');
-      data = await getData(true);
+      data = await getData(username, password, true);
     }
     if (data) {
       const store = await getStore();
