b23c7f3aa9b60034a973a2ec0baafe88b6a0a2e9
zhangxin <812603872@qq.com>
2023-05-27 22:40:58 +0800

    fix

--- code/src/util.ts
+++ code/src/util.ts
@@ -440,7 +440,7 @@ export const saveStore = async (s: Partial<Store>, updateTimestamp = true) => {
   // 本地先存
   const path = './data/store.json';
   const store = await getStore();
-  const tStore: Partial<Store> = updateTimestamp ? { ...store, ...s, timestamp: dayjs().valueOf() } : { ...store, ...s };
+  const tStore: Partial<Store> = { ...store, ...s, timestamp: dayjs().valueOf() }
   fs.writeFileSync(path, Format(tStore), { encoding: 'utf-8' });
   // oss保存
   try {
