5f5d1c2d57db6fbb63dbfd2e521a5e239a32ed9a
zhangxin <812603872@qq.com>
2023-07-08 17:51:13 +0800

    fix

--- code/infoIndex.js
+++ code/infoIndex.js
@@ -1,17 +1,19 @@
 const _fetch = import('node-fetch');
 
-exports.handler = async () => {
-  const fetch = (await _fetch).default;
-  fetch("http://todolist.web-framework.1048992591952509.cn-hangzhou.fc.devsapp.net/data?p=trigger_123@", {
-    "headers": {
-      "accept": "*/*",
-      "accept-language": "zh-CN,zh;q=0.9",
-      "proxy-connection": "keep-alive",
-      "cookie": "password=test_123@",
-      "Referer": "http://todolist.web-framework.1048992591952509.cn-hangzhou.fc.devsapp.net/?spm=5176.fcnext.0.0.56c878c82a6wDw",
-      "Referrer-Policy": "strict-origin-when-cross-origin"
-    },
-    "body": null,
-    "method": "GET"
-  });
+exports.handler = () => {
+  _fetch.then((_) => {
+    const fetch = _.default
+    fetch("http://todolist.web-framework.1048992591952509.cn-hangzhou.fc.devsapp.net/data?p=trigger_123@", {
+      "headers": {
+        "accept": "*/*",
+        "accept-language": "zh-CN,zh;q=0.9",
+        "proxy-connection": "keep-alive",
+        "cookie": "password=test_123@",
+        "Referer": "http://todolist.web-framework.1048992591952509.cn-hangzhou.fc.devsapp.net/?spm=5176.fcnext.0.0.56c878c82a6wDw",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+      },
+      "body": null,
+      "method": "GET"
+    });
+  })
 }
