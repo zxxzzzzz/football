d516f6b88bf9b5616a44a411c59440ca7da56139
zhangxin <812603872@qq.com>
2023-11-05 21:04:54 +0800

    fix

--- code/src/util.ts
+++ code/src/util.ts
@@ -12,20 +12,8 @@ let client: OSS | undefined = void 0;
     // yourRegion填写Bucket所在地域。以华东1（杭州）为例，Region填写为oss-cn-hangzhou。
     region: 'oss-cn-hangzhou',
     // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-    accessKeyId: [
-      'u', 'x', 'U', '7', 'M',
-      '7', 'K', 'A', 'c', 'E',
-      'T', 'c', 'x', '9', 'y',
-      'S', 'p', 'N', 't', '5',
-      'I', 'A', 'T', 'L'
-    ].reverse().join(''),
-    accessKeySecret: [
-      'o', 'q', 'l', 'a', 't', 'T',
-      'Z', 'U', 's', 's', 'M', 'g',
-      'J', 'q', 'H', 'Z', '5', 'T',
-      'D', 's', 'O', 'm', 'C', 'V',
-      'U', 'Q', '1', 'w', 'J', 'x'
-    ].reverse().join(),
+    accessKeyId: 'LTAI5tNpSy9xc'+ 'TEcAK7M7Uxu',
+    accessKeySecret: 'xJw1QUVCmOs'+'DT5ZHqJgMssUZTtalqo',
     bucket: 'footballc',
     internal: true,
   });
