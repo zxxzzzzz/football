5271c5854605883f60d825335fe1fdc872ded6e5
zhangxin <812603872@qq.com>
2023-11-12 22:56:43 +0800

    fix

--- code/update.js
+++ code/update.js
@@ -1,7 +1,7 @@
 const { getData } = require('./dist/getFootBall');
 exports.handler = async (event, context, callback) => {
   try {
-    await getData('peng902', 'Jxd9061912')
+    // await getData('peng902', 'Jxd9061912')
     callback(null, {
       statusCode: 500,
       body: 'success',
