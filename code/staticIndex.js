3939899795229b03acdeea969c74abc88bed27de
zhangxin <812603872@qq.com>
2023-11-12 17:39:36 +0800

    fix

--- code/staticIndex.js
+++ code/staticIndex.js
@@ -4,24 +4,27 @@ const { createHash } = require('crypto');
 
 exports.handler = (event, context, callback) => {
   try {
+    const rawPath = event.rawPath || '';
     const hash = createHash('sha256');
-    const eventObj = JSON.parse(event);
-
-    let body = 'Hello World!';
-    // get http request body
-    if ('body' in eventObj) {
-      body = eventObj.body;
-      if (eventObj.isBase64Encoded) {
-        body = Buffer.from(body, 'base64').toString('utf-8');
-      }
+    let responseBody = '';
+    let contentType = 'text/html;charset=UTF-8';
+    if (!rawPath || rawPath === '/') {
+      responseBody = fs.readFileSync(path.resolve(__dirname, './public/index.html'), { encoding: 'utf-8' });
+    } else {
+      responseBody = fs.readFileSync(path.resolve(__dirname, './public' + rawPath), { encoding: 'utf-8' });
     }
-    const html = fs.readFileSync(path.resolve(__dirname, './public/index.html'), { encoding: 'utf-8' });
-    const hashText = hash.update(html, 'utf-8').digest('hex');
+    if (rawPath.endsWith('.js')) {
+      contentType = 'application/x-javascript';
+    }
+    if (rawPath.endsWith('.css')) {
+      contentType = 'text/css';
+    }
+    const hashText = hash.update(responseBody, 'utf-8').digest('hex');
     callback(null, {
       statusCode: 200,
-      body: html,
+      body: responseBody,
       headers: {
-        'Content-Type': 'text/html;charset=UTF-8',
+        'Content-Type': contentType,
         'Cache-Control': 'no-cache',
         ETag: hashText,
       },
