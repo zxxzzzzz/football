f3d3716aa170a52a555a859f9c28063b9b812fd4
zhangxin <812603872@qq.com>
2023-11-12 20:38:03 +0800

    fix56

--- code/index.js
+++ code/index.js
@@ -3,12 +3,12 @@ const { saveStore } = require('./dist/util');
 
 exports.handler = async (event, context, callback) => {
   const eventObj = JSON.parse(event.toString());
-  const password = eventObj?.queryParameters?.p || '';
-  const token = eventObj?.queryParameters?.token || '';
-  const responseData = getDataByHttp({ password, token });
+  // const password = eventObj?.queryParameters?.p || '';
+  // const token = eventObj?.queryParameters?.token || '';
+  // const responseData = getDataByHttp({ password, token });
   callback({
     statusCode: 200,
-    body: responseData,
+    body: 'data',
     headers: {
       'Content-Type': 'application/json',
       'Cache-Control': 'no-cache',
