ed7af513d97637ddc2fb9bd880346c5b9810eb63
zhangxin <812603872@qq.com>
2023-11-12 16:25:43 +0800

    fix

--- code/index.js
+++ code/index.js
@@ -1,14 +1,15 @@
-const serverless = require('@serverless-devs/fc-http');
+const serverless = require('serverless-http');
+
 const { default: app } = require('./dist/index');
 const { saveStore } = require('./dist/util');
 
-exports.handler = serverless(app);
-exports.infoHandler = () => {
-  console.log(123);
-  return true
-};
 
-module.exports.preStop = function (context, callback) {
+
+// exports.handler = ()=>{
+
+// };
+
+exports.preStop = function (context, callback) {
   saveStore({}, true).then(() => {
     console.log('销毁前上传数据');
     callback(null, '');
---
+++ code/staticIndex.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+
+exports.handler = (event, context, callback) => {
+  const eventObj = JSON.parse(event);
+
+  let body = 'Hello World!';
+  // get http request body
+  if ("body" in eventObj) {
+    body = eventObj.body;
+    if (eventObj.isBase64Encoded) {
+      body = Buffer.from(body, 'base64').toString('utf-8');
+    }
+  }
+  const html = fs.readFileSync('./public/index.html', {encoding:'utf-8'})
+  callback(null, {
+    'statusCode': 200,
+    'body': html
+  });
+}
