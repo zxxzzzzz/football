17cd205ba72eb05b79a2fc2cd7b5cc32f4a181ef
zhangxin <812603872@qq.com>
2023-11-12 17:06:01 +0800

    fix

--- code/staticIndex.js
+++ code/staticIndex.js
@@ -1,8 +1,8 @@
 import fs from 'fs';
 import path from 'path';
-import {createHash } from 'crypto';
+import { createHash } from 'crypto';
 
-const hash = createHash('sha256')
+const hash = createHash('sha256');
 exports.handler = (event, context, callback) => {
   const eventObj = JSON.parse(event);
 
@@ -14,13 +14,13 @@ exports.handler = (event, context, callback) => {
       body = Buffer.from(body, 'base64').toString('utf-8');
     }
   }
-  const html = fs.readFileSync(path.resolve(__dirname, './public/index.html'), { encoding: 'utf-8' });
-  const hashText  = hash.update(html, 'utf-8').digest('hex')
+  const html = fs.readFileSync(path.resolve(__dirname, '../public/index.html'), { encoding: 'utf-8' });
+  const hashText = hash.update(html, 'utf-8').digest('hex');
   callback(null, {
     statusCode: 200,
     body: html,
     'content-type': 'text/html;charset=UTF-8',
     'Cache-Control': 'no-cache',
-    'ETag': hashText
+    ETag: hashText,
   });
 };
