4d401cec503ec7e7ba6e31acbdd00d865bd792af
zhangxin <812603872@qq.com>
2024-03-07 19:41:19 +0800

    fix

--- code/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getDataByHttp = void 0;
-var getFootBall_1 = require("./getFootBall");
-Object.defineProperty(exports, "getDataByHttp", { enumerable: true, get: function () { return getFootBall_1.getDataByHttp; } });
